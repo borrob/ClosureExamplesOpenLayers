393d90eb4df0ff2e34816da697613f1025e5dc90
Rob van Loon <rob.van.loon@ordina.nl>
2017-06-16 21:43:16 +0200

    New builds

--- 05_HelloOL/src/map.js
+++ 05_HelloOL/src/map.js
@@ -6,17 +6,16 @@ goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
 
 App.Map.init = function() {
-App.Map.map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  view: new ol.View({
-	center: [620000,6732600],
-	zoom: 7
-  })
-});
+	App.Map.map = new ol.Map({
+		target: 'map',
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		],
+		view: new ol.View({
+			center: [620000,6732600],
+			zoom: 7
+		})
+	});
 }
-
